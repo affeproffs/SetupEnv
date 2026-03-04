a4c6c78676f60ea3ac7070ec257c0c4d1f20d49b
affeproffs <astenbor@hotmail.com>
2026-03-04 14:46:36 +0100

    feat: install gh

--- 2.sh
+++ 2.sh
@@ -31,11 +31,12 @@ main() {
     installOpenClaw;
 
     # Applications
+    installGh;
+    installCodex;
     installVsCode;
     installSpotify;
     install1Password;
-    installChrome;
-    installCodex;
+    installChrome;    
 
     echo -e "${GREEN}Installations complete.${NOCOLOR}"
     exit 0;
@@ -501,6 +502,27 @@ installK9s() {
     echo "Installing K9s..."
     brew install derailed/k9s/k9s
     echo -e "${GREEN}Finished installing Homebrew.${NOCOLOR}"
+    sleep 3;
+}
+
+installGh() {
+    clear
+    if ! askForInstall "GitHub CLI"; then
+        return;
+    fi
+
+    echo "Installing GitHub CLI..."
+    cd ~/Downloads
+    (type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+    echo -e "${GREEN}Finished installing GitHub CLI.${NOCOLOR}"
     sleep 3;
 }
 
